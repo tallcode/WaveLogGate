2e0c795b8a969f2304ef8c54e05475bc00a5ecdb
Tao Xu <angelxutao@gmail.com>
2024-11-01 22:09:31 +0800

    Update forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -10,7 +10,7 @@ module.exports = {
 			name: '@electron-forge/publisher-github',
 			config: {
 				repository: {
-					owner: 'wavelog',
+					owner: 'tallcode',
 					name: 'WaveLogGate'
 				},
 				prerelease: false
